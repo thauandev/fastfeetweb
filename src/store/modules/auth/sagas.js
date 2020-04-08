8631dfa88ca999dab95cf48a1ffd204f6d9defb5
thauandev <thauandepaula@icloud.com>
2020-04-08 10:02:05 -0300

    Implementando a parte de login

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,23 @@
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+
+import history from '~/services/history';
+import api from '~/services/api';
+
+import { signInSucces } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  yield put(signInSucces(token, user));
+
+  history.push('/delivery');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
