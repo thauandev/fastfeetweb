50805529467d7569b116523797177cd0258bd2f7
thauandev <thauandepaula@icloud.com>
2020-04-08 11:41:19 -0300

    Requisições autenticadas

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -21,7 +21,7 @@ export default function auth(state = INITIAL_STATE, action) {
         break;
       }
 
-      case '@auth/SIGN_IN_FAILURE': {
+      case '@auth/SIGN_FAILURE': {
         draft.loading = false;
         break;
       }
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -16,11 +17,28 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSucces(token, user));
 
     history.push('/delivery');
   } catch (err) {
+    toast.error('Falha na autenticação verifique seus dados');
     yield put(signFailure());
   }
 }
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);
